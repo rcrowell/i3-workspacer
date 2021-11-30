8c2823e09e97b22c138f96410d3d450bf0cea967
Rob Crowell <robccrowell@gmail.com>
2021-11-29 16:11:05 -0800

    add rudimentary argparse

--- workspacer/i3.py
+++ workspacer/i3.py
@@ -61,7 +61,13 @@ class Workspace:
       return workspaces[next_index]
     # Create new workspace here!
     return Workspace(name=str(next_digit), is_created=False)
-      
+
+  def make_current(self):
+    subprocess.run(['i3-msg', 'workspace {name}'.format(name=self.name)])
+
+  def move_container(self):    
+    subprocess.run(['i3-msg', 'move container to workspace {name}'.format(name=self.name)])
+
 
 def fetch_workspaces():
   result = subprocess.run(['i3-msg', '-t', 'get_workspaces'], capture_output=True)
@@ -73,7 +79,45 @@ def current_workspace(workspaces):
   return [x for x in workspaces if x.is_visible and x.is_focused][0]
 
 
+class Dispatcher:
+  @staticmethod
+  def handle_all(args):
+    workspaces = fetch_workspaces()
+    print(workspaces)
+    
+  @staticmethod  
+  def handle_current(args):
+    workspaces = fetch_workspaces()
+    workspace = current_workspace(workspaces=workspaces)
+    print(workspace)
+
+  @staticmethod
+  def handle_next(args):
+    workspaces = fetch_workspaces()
+    workspace = current_workspace(workspaces=workspaces)
+    next_workspace = workspace.next_workspace(workspaces=workspaces)
+    if args.move_container:
+      next_workspace.move_container()
+    if args.make_current:
+      next_workspace.make_current()
+    print(next_workspace)
+
+
 if __name__ == '__main__':
-  workspaces = fetch_workspaces()
-  workspace = current_workspace(workspaces=workspaces)
-  print(workspace.next_workspace(workspaces=workspaces))
+  parser = argparse.ArgumentParser()
+  parser.set_defaults(fn=Dispatcher.handle_all)
+  subparsers = parser.add_subparsers()
+  # Args: all
+  all_p = subparsers.add_parser('all')
+  all_p.set_defaults(fn=Dispatcher.handle_all)
+  # Args: current
+  current_p = subparsers.add_parser('current')
+  current_p.set_defaults(fn=Dispatcher.handle_current)
+  # Args: next
+  next_p = subparsers.add_parser('next')
+  next_p.set_defaults(fn=Dispatcher.handle_next)
+  next_p.add_argument('--make-current', default=False, action='store_true')
+  next_p.add_argument('--move-container', default=False, action='store_true')
+  # Run the requested fn.
+  args = parser.parse_args()
+  args.fn(args)
