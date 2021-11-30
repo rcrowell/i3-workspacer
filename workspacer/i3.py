d95807a4a3fa26a9b1d748a5a10d921bc39a814a
Rob Crowell <robccrowell@gmail.com>
2021-11-29 18:32:00 -0800

    simplify by planning first

--- workspacer/i3.py
+++ workspacer/i3.py
@@ -37,34 +37,6 @@ class Workspace:
       ])
     return False
 
-  def _successor_workspace(self, workspaces, digits, to_successor_index):
-    my_digit = int(self.name) if self.name.isdigit() else None
-    successor_digit = digits[to_successor_index(my_digit, digits)] if my_digit in digits else None
-    created_digits = set(int(x.name) for x in workspaces if x.is_created and x.name.isdigit())
-    if ((not digits)
-        or (my_digit is None)
-        or (my_digit not in digits)
-        or (successor_digit in created_digits)):
-      # Cannot create unless self is in digits and next_digit is not created yet.
-      next_index = to_successor_index(self, workspaces)
-      return workspaces[next_index]
-    # Create a workspace for successor_digit.
-    return Workspace(name=str(successor_digit), is_created=False)
-
-  def prev_workspace(self, workspaces, digits=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)):
-    return self._successor_workspace(
-        workspaces=workspaces,
-        digits=digits,
-        to_successor_index=lambda digit, digits: (digits.index(digit) - 1) % len(digits),
-    )
-
-  def next_workspace(self, workspaces, digits=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)):
-    return self._successor_workspace(
-        workspaces=workspaces,
-        digits=digits,
-        to_successor_index=lambda digit, digits: (digits.index(digit) + 1) % len(digits),
-    )
-      
   def make_current(self):
     subprocess.run(['i3-msg', 'workspace {name}'.format(name=self.name)],
                    capture_output=True)
@@ -74,20 +46,57 @@ class Workspace:
                    capture_output=True)
 
 
-def fetch_workspaces():
+def fetch_workspaces(managed_names=()):
   result = subprocess.run(['i3-msg', '-t', 'get_workspaces'], capture_output=True)
   result = json.loads(result.stdout)
-  return [Workspace.from_js(x) for x in result]
+  workspaces = [Workspace.from_js(x) for x in result]
+  # Ensure all managed_names appear in workspaces, even if they have not been created yet.
+  managed_names = [str(x) for x in managed_names]
+  for i, managed_name in enumerate(managed_names):
+    created = [x.name for x in workspaces]
+    if managed_name not in created:
+      if i == 0:
+        # Insert right before the smallest managed_name that exists, or at the end.
+        index, _ = min(
+            [(i, x) for i, x in enumerate(created) if x in managed_names],
+            key=lambda x: managed_names.index(x[1]),
+            default=(len(workspaces), None),
+        )
+      else:
+        # Insert right after the previous managed_name.
+        prev_name = managed_names[i-1]
+        index = 1 + ([i for i, x in enumerate(workspaces) if x.name == prev_name][0])
+      workspaces.insert(index, Workspace(name=managed_name, is_created=False))
+  return workspaces
 
 
 def current_workspace(workspaces):
   return [x for x in workspaces if x.is_visible and x.is_focused][0]
 
 
+def prev_workspace(workspaces, workspace=None):
+  workspace = workspace or current_workspace(workspaces=workspaces)
+  prev_index = (workspaces.index(workspace) - 1) % len(workspaces)
+  return workspaces[prev_index]
+
+
+def next_workspace(workspaces, workspace=None):
+  workspace = workspace or current_workspace(workspaces=workspaces)
+  next_index = (workspaces.index(workspace) + 1) % len(workspaces)
+  return workspaces[next_index]
+
+
 class Dispatcher:
   @staticmethod
+  def _to_digits(min_digit=None, max_digit=None):
+    if (min_digit is None) ^ (max_digit is None):
+      raise ValueError('min_digit and max_digit must both be set or unset')
+    return range(min_digit, max_digit+1) if min_digit is not None else ()
+        
+  @staticmethod
   def handle_all(args):
-    workspaces = fetch_workspaces()
+    digits = Dispatcher._to_digits(min_digit=args.min_digit, max_digit=args.max_digit)
+    workspaces = fetch_workspaces(managed_names=digits)
     print(workspaces)
     
   @staticmethod  
@@ -98,51 +107,51 @@ class Dispatcher:
 
   @staticmethod
   def handle_prev(args):
-    digits = range(args.min_digit, args.max_digit+1)
-    workspaces = fetch_workspaces()
-    workspace = current_workspace(workspaces=workspaces)
-    prev_workspace = workspace.prev_workspace(workspaces=workspaces, digits=digits)
+    digits = Dispatcher._to_digits(min_digit=args.min_digit, max_digit=args.max_digit)
+    workspaces = fetch_workspaces(managed_names=digits)
+    prev_workspace_ = prev_workspace(workspaces=workspaces)
     if args.move_container:
-      prev_workspace.move_container()
+      prev_workspace_.move_container()
     if args.make_current:
-      prev_workspace.make_current()
-    print(prev_workspace)
+      prev_workspace_.make_current()
+    print(prev_workspace_)
 
   @staticmethod
   def handle_next(args):
-    digits = range(args.min_digit, args.max_digit+1)
-    workspaces = fetch_workspaces()
-    workspace = current_workspace(workspaces=workspaces)
-    next_workspace = workspace.next_workspace(workspaces=workspaces, digits=digits)
+    digits = Dispatcher._to_digits(min_digit=args.min_digit, max_digit=args.max_digit)
+    workspaces = fetch_workspaces(managed_names=digits)
+    next_workspace_ = next_workspace(workspaces=workspaces)
     if args.move_container:
-      next_workspace.move_container()
+      next_workspace_.move_container()
     if args.make_current:
-      next_workspace.make_current()
-    print(next_workspace)
+      next_workspace_.make_current()
+    print(next_workspace_)
     
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.set_defaults(fn=Dispatcher.handle_all)
+  parser.set_defaults(fn=Dispatcher.handle_current)
   subparsers = parser.add_subparsers()
-  # Args: all
-  all_p = subparsers.add_parser('all')
-  all_p.set_defaults(fn=Dispatcher.handle_all)
   # Args: current
   current_p = subparsers.add_parser('current')
   current_p.set_defaults(fn=Dispatcher.handle_current)
+  # Args: all
+  all_p = subparsers.add_parser('all')
+  all_p.set_defaults(fn=Dispatcher.handle_all)
+  all_p.add_argument('--min-digit', type=int, default=None)
+  all_p.add_argument('--max-digit', type=int, default=None)
   # Args: prev
   prev_p = subparsers.add_parser('prev')
   prev_p.set_defaults(fn=Dispatcher.handle_prev)
-  prev_p.add_argument('--min-digit', type=int, default=1)
-  prev_p.add_argument('--max-digit', type=int, default=10)
+  prev_p.add_argument('--min-digit', type=int, default=None)
+  prev_p.add_argument('--max-digit', type=int, default=None)
   prev_p.add_argument('--make-current', default=False, action='store_true')
   prev_p.add_argument('--move-container', default=False, action='store_true')
   # Args: next
   next_p = subparsers.add_parser('next')
   next_p.set_defaults(fn=Dispatcher.handle_next)
-  next_p.add_argument('--min-digit', type=int, default=1)
-  next_p.add_argument('--max-digit', type=int, default=10)
+  next_p.add_argument('--min-digit', type=int, default=None)
+  next_p.add_argument('--max-digit', type=int, default=None)
   next_p.add_argument('--make-current', default=False, action='store_true')
   next_p.add_argument('--move-container', default=False, action='store_true')
   # Run the requested fn.
