0cd2ec2d96736115baa4c7ff96fdc1a6c7d8b915
Rob Crowell <robccrowell@gmail.com>
2021-11-29 16:47:25 -0800

    more generic next handling

--- workspacer/i3.py
+++ workspacer/i3.py
@@ -37,36 +37,35 @@ class Workspace:
       ])
     return False
 
-  def next_workspace(self, workspaces, max_digit=9):
-    my_index = workspaces.index(self)
-    next_index = (my_index + 1) % len(workspaces)
-    if max_digit is None:
-      # May not create new workspaces; just return the next via round-robin.
+  def _successor_workspace(self, workspaces, digits, to_successor_index):
+    my_digit = int(self.name) if self.name.isdigit() else None
+    successor_digit = digits[to_successor_index(my_digit, digits)] if my_digit in digits else None
+    created_digits = set(int(x.name) for x in workspaces if x.is_created and x.name.isdigit())
+    if ((not digits)
+        or (my_digit is None)
+        or (my_digit not in digits)
+        or (successor_digit in created_digits)):
+      # Cannot create unless self is in digits and next_digit is not created yet.
+      my_index = workspaces.index(self)
+      next_index = (my_index + 1) % len(workspaces)
       return workspaces[next_index]
-    if self.name.isdigit():
-      next_digit = (int(self.name) + 1) % (max_digit + 1)
-    else:
-      all_digits = set(int(x.name) for x in workspaces if x.name.isdigit())
-      for i in range(max_digit + 1):
-        next_digit = (i + 1) % (max_digit + 1)
-        if next_digit not in all_digits:
-          break
-    # May not create workspace if next_digit already exists someplace in workspaces.
-    digit_matches = [x for x in workspaces if x.name == str(next_digit)]
-    if digit_matches:
-      return workspaces[next_index]
-    # May not create workspace if self.name != (next_digit - 1) modulo max_digit.
-    prev_digit = (next_digit - 1) % (max_digit + 1)
-    if (self.name != str(prev_digit)):
-      return workspaces[next_index]
-    # Create new workspace here!
-    return Workspace(name=str(next_digit), is_created=False)
+    # Create a workspace for successor_digit.
+    return Workspace(name=str(successor_digit), is_created=False)
 
+  def next_workspace(self, workspaces, digits=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)):
+    return self._successor_workspace(
+        workspaces=workspaces,
+        digits=digits,
+        to_successor_index=lambda digit, digits: (digits.index(digit) + 1) % len(digits),
+    )
+      
   def make_current(self):
-    subprocess.run(['i3-msg', 'workspace {name}'.format(name=self.name)])
+    subprocess.run(['i3-msg', 'workspace {name}'.format(name=self.name)],
+                   capture_output=True)
 
   def move_container(self):    
-    subprocess.run(['i3-msg', 'move container to workspace {name}'.format(name=self.name)])
+    subprocess.run(['i3-msg', 'move container to workspace {name}'.format(name=self.name)],
+                   capture_output=True)
 
 
 def fetch_workspaces():
@@ -93,9 +92,10 @@ class Dispatcher:
 
   @staticmethod
   def handle_next(args):
+    digits = range(args.min_digit, args.max_digit+1)
     workspaces = fetch_workspaces()
     workspace = current_workspace(workspaces=workspaces)
-    next_workspace = workspace.next_workspace(workspaces=workspaces)
+    next_workspace = workspace.next_workspace(workspaces=workspaces, digits=digits)
     if args.move_container:
       next_workspace.move_container()
     if args.make_current:
@@ -116,6 +116,8 @@ if __name__ == '__main__':
   # Args: next
   next_p = subparsers.add_parser('next')
   next_p.set_defaults(fn=Dispatcher.handle_next)
+  next_p.add_argument('--min-digit', type=int, default=1)
+  next_p.add_argument('--max-digit', type=int, default=10)
   next_p.add_argument('--make-current', default=False, action='store_true')
   next_p.add_argument('--move-container', default=False, action='store_true')
   # Run the requested fn.
